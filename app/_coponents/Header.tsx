c4cf9e97a753b9be252aae119fa48846451f3984
hoinlee-moi <lhi3048@gmail.com>
2024-02-28 18:47:02 +0900

    first commit

---
+++ app/_coponents/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <nav className="relative h-12 p-2 rounded-br-md rounded-bl-md bg-sky-200 w-full">
+      <h1 className="text-2xl font-bold text-white">My Album</h1>
+    </nav>
+  );
+}
