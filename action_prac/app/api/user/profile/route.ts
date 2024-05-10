2cbc77010ee3cf9a12e812026d0e985bfc0efe4d
hoinlee-moi <lhi3048@gmail.com>
2024-05-10 16:37:11 +0900

    test2

--- action_prac/app/api/user/profile/route.ts
+++ action_prac/app/api/user/profile/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import React from "react";
 
-export default function GET() {
+export async function GET() {
   const user = { name: "hoi", location: "seoul" };
 
   return NextResponse.json(user, { status: 200 });
